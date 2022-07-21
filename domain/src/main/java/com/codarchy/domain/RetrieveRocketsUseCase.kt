6f0ed73bc928ca6638ea017ed386187dfd8dcd9b
Peter Szasz <szasz.peter@ymail.com>
2022-07-21 14:52:30 +0200

    Domain Module basic functionality
    Based on the blueprint that I had here, I generated a simple Use Case that will be responsible for calling the Data Layer to bring in the necessary information

---
+++ domain/src/main/java/com/codarchy/domain/RetrieveRocketsUseCase.kt
@@ -0,0 +1,12 @@
+package com.codarchy.domain
+
+import com.codarchy.data.errorhandling.ResultWrapper
+import com.codarchy.data.model.Rocket
+import com.codarchy.data.repository.RocketRepository
+import javax.inject.Inject
+
+class RetrieveRocketsUseCase @Inject constructor(private val repository: RocketRepository){
+    suspend operator fun invoke(): ResultWrapper<List<Rocket>> {
+        return repository.requestRocketList()
+    }
+}
